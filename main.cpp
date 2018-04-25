262aed4d1bced264bf5896e158368ec82809b12c
Jesus Fernandez <jesus.fernandez@qt.io>
2018-04-25 18:36:08 +0200

    Parse JSON

--- main.cpp
+++ main.cpp
@@ -3,6 +3,42 @@
 
 static QNetworkAccessManager networkAccessManager;
 
+void parseJson(const QByteArray &data)
+{
+    QJsonParseError parseError;
+    const auto document = QJsonDocument::fromJson(data, &parseError);
+    if (parseError.error) {
+        qCritical() << "TwitterTimelineModel::parseJson. Error at:" << parseError.offset
+                    << parseError.errorString();
+        return;
+    } else if (document.isObject()) {
+        // Error received :(
+        const auto object = document.object();
+        const auto errorArray = object.value("errors").toArray();
+        if (errorArray.size() > 0){
+            QStringList errors;
+            for (const auto error : errorArray) {
+                Q_ASSERT(error.isObject());
+                Q_ASSERT(error.toObject().contains("message"));
+                errors.append(error.toObject().value("message").toString());
+            }
+            qCritical() << errors.join("<br />");
+            return;
+        } else if (object.contains("statuses")) {
+            const auto array = object.value("statuses").toArray();
+            if (array.isEmpty())
+                return;
+            for (const auto &value : array) {
+                Q_ASSERT(value.isObject());
+                const auto object = value.toObject();
+                const auto user = object.value("user").toObject().value("screen_name").toString();
+                qDebug() << "Tweet: " << object.value("user").toObject().value("screen_name")
+                         << ":" << object.value("text");
+            }
+        }
+    }
+}
+
 void request()
 {
     QUrl url("https://api.twitter.com/1.1/search/tweets.json");
@@ -18,7 +54,7 @@ void request()
 
     auto reply = networkAccessManager.get(request);
     QObject::connect(reply, &QNetworkReply::finished, [reply]() {
-        qDebug() << reply->readAll();
+        parseJson(reply->readAll());
         qApp->quit();
     });
     QObject::connect(reply, qOverload<QNetworkReply::NetworkError>(&QNetworkReply::error),
